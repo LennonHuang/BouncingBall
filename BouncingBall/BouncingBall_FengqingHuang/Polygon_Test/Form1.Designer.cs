b2828dc593a5b53a721cc90fc99534d5b5114b34
LennonHuang <62449961+LennonHuang@users.noreply.github.com>
2020-10-12 11:06:41 -0700

    Add files via upload

---
+++ BouncingBall/BouncingBall_FengqingHuang/Polygon_Test/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Polygon_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.polygonView = new Polygon_Test.PolygonView();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(796, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "5";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(880, 62);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(90, 423);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(820, 542);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelColor
+            // 
+            this.labelColor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelColor.Location = new System.Drawing.Point(820, 604);
+            this.labelColor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(160, 37);
+            this.labelColor.TabIndex = 4;
+            this.labelColor.Click += new System.EventHandler(this.labelColor_Click);
+            // 
+            // polygonView
+            // 
+            this.polygonView.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.polygonView.Color = System.Drawing.Color.DarkGreen;
+            this.polygonView.Location = new System.Drawing.Point(22, 19);
+            this.polygonView.Margin = new System.Windows.Forms.Padding(4);
+            this.polygonView.Name = "polygonView";
+            this.polygonView.Size = new System.Drawing.Size(758, 725);
+            this.polygonView.TabIndex = 0;
+            this.polygonView.velocity = 5;
+            this.polygonView.Load += new System.EventHandler(this.polygonView_Load);
+            this.polygonView.Paint += new System.Windows.Forms.PaintEventHandler(this.polygonView_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(992, 769);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.polygonView);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PolygonView polygonView;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelColor;
+    }
+}
+
---
+++ BouncingBall/BouncingBall_FengqingHuang/Polygon_Test/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polygon_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelColor.BackColor = polygonView.Color;
+            colorDialog1.Color = polygonView.Color;
+        }
+
+        //Set the velocity in the textbox to the ball
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int velocity;
+            //in case the user sets the textbox blank.
+            if (textBox1.Text == string.Empty) { velocity = 0; }
+            else
+            {
+                //otherwise send the textbox content.
+                velocity = Convert.ToInt16(textBox1.Text);
+            }
+            polygonView.velocity = velocity;
+            //send the velocity to the trackBar. 
+            if (velocity > trackBar1.Maximum) velocity = trackBar1.Maximum;
+            trackBar1.Value = velocity;
+            
+        }
+
+        //send the velocity in the trackBar to the UserController.
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int velocity = trackBar1.Value;
+            polygonView.velocity = velocity;
+            textBox1.Text = velocity.ToString();
+        }
+
+        //Colour Control.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                polygonView.Color = colorDialog1.Color;
+                labelColor.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void polygonView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void polygonView_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void labelColor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
